932c896623b946317c8de3b7c0bdd8a666e507a3
boringhexi <boringhexi@pm.me>
2023-11-29 23:52:06 -0500

    ghs_modelmeta_extract: give character shadow ghs files a different extension

--- mymodules/ghsmodelmetaoutloc.py
+++ mymodules/ghsmodelmetaoutloc.py
@@ -4,57 +4,60 @@ from typing import Optional
 
 characters = {
     0x00: ("02c.sli.tex", "boy"),
-    0x01: ("02c.sli.tex", "boy_shadow"),
     0x02: ("02e.sli.tex", "girl"),
-    0x03: ("02e.sli.tex", "girl_shadow"),
     0x04: ("0aa.stm/000.sli.tex", "gregory"),
-    0x05: ("0aa.stm/000.sli.tex", "gregory_shadow"),
     0x06: ("0aa.stm/002.sli.tex", "judgementboy"),
-    0x07: ("0aa.stm/002.sli.tex", "judgementboy_shadow"),
     0x08: ("0aa.stm/004.sli.tex", "nekozombie"),
-    0x09: ("0aa.stm/004.sli.tex", "nekozombie_shadow"),
     0x0A: ("0aa.stm/006.sli.tex", "lostdoll"),
-    0x0B: ("0aa.stm/006.sli.tex", "lostdoll_shadow"),
     0x0C: ("0aa.stm/008.sli.tex", "hellschef"),
-    0x0D: ("0aa.stm/008.sli.tex", "hellschef_shadow"),
     0x0E: ("0aa.stm/00a.sli.tex", "catherine"),
-    0x0F: ("0aa.stm/00a.sli.tex", "catherine_shadow"),
     0x10: ("0aa.stm/00c.sli.tex", "mummypapa"),
-    0x11: ("0aa.stm/00c.sli.tex", "mummypapa_shadow"),
     0x12: ("0aa.stm/00e.sli.tex", "mummydog"),
-    0x13: ("0aa.stm/00e.sli.tex", "mummydog_shadow"),
     0x14: ("0aa.stm/010.sli.tex", "cactusgunman"),
-    0x15: ("0aa.stm/010.sli.tex", "cactusgunman_shadow"),
     0x16: ("0aa.stm/012.sli.tex", "cactusgirl"),
-    0x17: ("0aa.stm/012.sli.tex", "cactusgirl_shadow"),
     0x18: ("0aa.stm/014.sli.tex", "tvfish"),
-    0x19: ("0aa.stm/014.sli.tex", "tvfish_shadow"),
     0x1A: ("0aa.stm/016.sli.tex", "clockmaster"),
-    0x1B: ("0aa.stm/016.sli.tex", "clockmaster_shadow"),
     0x1C: ("0aa.stm/018.sli.tex", "myson"),
-    0x1D: ("0aa.stm/018.sli.tex", "myson_shadow"),
     0x1E: ("0aa.stm/01a.sli.tex", "rouletteboy"),
-    0x1F: ("0aa.stm/01a.sli.tex", "rouletteboy_shadow"),
     0x20: ("0aa.stm/01c.sli.tex", "angeldog"),
-    0x21: ("0aa.stm/01c.sli.tex", "angeldog_shadow"),
     0x22: ("0aa.stm/01e.sli.tex", "judgementboygold"),
-    0x23: ("0aa.stm/01e.sli.tex", "judgementboygold_shadow"),
     0x24: ("0b0.sli.tex", "gregorymama"),
-    0x25: ("0b0.sli.tex", "gregorymama_shadow"),
     0x26: ("0aa.stm/022.sli.tex", "james"),
-    0x27: ("0aa.stm/022.sli.tex", "james_shadow"),
     0x28: ("0b4.sli.tex", "gregoryghost"),
-    0x29: ("0b4.sli.tex", "gregoryghost_shadow"),
     0x2A: ("0aa.stm/000.sli.tex", "gregory_duplicate"),
-    0x2B: ("0aa.stm/000.sli.tex", "gregory_duplicate_shadow"),
     0x2C: ("04d.sli.stm/009.sli.tex", "frogfortuneteller"),
-    0x2D: ("04d.sli.stm/009.sli.tex", "frogfortuneteller_shadow"),
     0x2E: ("0b2.sli.tex", "death"),
-    0x2F: ("0b2.sli.tex", "death_shadow"),
     0x30: ("0aa.stm/02c.sli.tex", "devildog"),
-    0x31: ("0aa.stm/02c.sli.tex", "devildog_shadow"),
     0x32: ("0aa.stm/000.sli.tex", "gregory_duplicate"),
-    0x33: ("0aa.stm/000.sli.tex", "gregory_duplicate_shadow"),
+}
+
+character_shadows = {
+    0x01: ("02c.sli.tex", "boy"),
+    0x03: ("02e.sli.tex", "girl"),
+    0x05: ("0aa.stm/000.sli.tex", "gregory"),
+    0x07: ("0aa.stm/002.sli.tex", "judgementboy"),
+    0x09: ("0aa.stm/004.sli.tex", "nekozombie"),
+    0x0B: ("0aa.stm/006.sli.tex", "lostdoll"),
+    0x0D: ("0aa.stm/008.sli.tex", "hellschef"),
+    0x0F: ("0aa.stm/00a.sli.tex", "catherine"),
+    0x11: ("0aa.stm/00c.sli.tex", "mummypapa"),
+    0x13: ("0aa.stm/00e.sli.tex", "mummydog"),
+    0x15: ("0aa.stm/010.sli.tex", "cactusgunman"),
+    0x17: ("0aa.stm/012.sli.tex", "cactusgirl"),
+    0x19: ("0aa.stm/014.sli.tex", "tvfish"),
+    0x1B: ("0aa.stm/016.sli.tex", "clockmaster"),
+    0x1D: ("0aa.stm/018.sli.tex", "myson"),
+    0x1F: ("0aa.stm/01a.sli.tex", "rouletteboy"),
+    0x21: ("0aa.stm/01c.sli.tex", "angeldog"),
+    0x23: ("0aa.stm/01e.sli.tex", "judgementboygold"),
+    0x25: ("0b0.sli.tex", "gregorymama"),
+    0x27: ("0aa.stm/022.sli.tex", "james"),
+    0x29: ("0b4.sli.tex", "gregoryghost"),
+    0x2B: ("0aa.stm/000.sli.tex", "gregory_duplicate"),
+    0x2D: ("04d.sli.stm/009.sli.tex", "frogfortuneteller"),
+    0x2F: ("0b2.sli.tex", "death"),
+    0x31: ("0aa.stm/02c.sli.tex", "devildog"),
+    0x33: ("0aa.stm/000.sli.tex", "gregory_duplicate"),
 }
 
 held_objects = {
@@ -121,12 +124,17 @@ def get_modelmeta_outloc(
     outsubdir = None
     outfilename = f"{modelmeta_i:03x}.ghs"
 
-    stuff = characters | held_objects | effects | rouletteboy_horrorshow | doors
-
-    if modelmeta_i in stuff:
-        outsubdir, namepart = stuff[modelmeta_i]
+    ghs_stuff = characters | held_objects | effects | rouletteboy_horrorshow | doors
+    if modelmeta_i in ghs_stuff:
+        outsubdir, namepart = ghs_stuff[modelmeta_i]
         if namepart:
             outfilename = f"{modelmeta_i:03x}_{namepart}.ghs"
+            
+    elif modelmeta_i in character_shadows:
+        outsubdir, namepart = character_shadows[modelmeta_i]
+        if namepart:
+            outfilename = f"{modelmeta_i:03x}_{namepart}.ghs-shadow"
+            
     elif stm_index != -1:  # room props that provide a .stm index
         outsubdir = f"{stm_index:03x}.sli.stm"
 
